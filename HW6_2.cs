006f0dd0f2acacf42e57e29bf7fe6fdd56a75699
IVANNNNN <gravemail@yandex.ru>
2022-08-29 14:30:04 +0300

    Homework 6_2

---
+++ HW6_2.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите b1: ");
+int b1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите k1: ");
+int k1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите b2: ");
+int b2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите k2: ");
+int k2 = int.Parse(Console.ReadLine()!);
+
+ 
+int x = -(b1 - b2) / (k1 - k2);
+int y = k1 * x + b1;
+
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
