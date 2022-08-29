bf0dd7896ce2413048cfc57a636a1e47422f27c2
IVANNNNN <gravemail@yandex.ru>
2022-08-29 14:29:54 +0300

    Homework 6_1

---
+++ HW6_1.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите колличество чисел: ");
+int numbers = int.Parse(Console.ReadLine()!);
+int count = 0;
+for (int i = 0; i < numbers; i++)
+{
+    Console.Write($"Число {i + 1}: ");
+     int number = int.Parse(Console.ReadLine()!);
+     if (number > 0)
+         count++;
+ }
+Console.WriteLine($"Количество чисел больше 0: {count}");
